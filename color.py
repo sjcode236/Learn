ab3eb546951152c18559f5c15c26f7276a12abd8
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-04-30 20:43:57 -0700

    Update and rename color.py to colors.py

--- color.py
+++
@@ -1,4 +0,0 @@
-
-This is colors  sample of.py
-\To high light remaining line
-@ to change colors of line 
---
+++ colors.py
@@ -0,0 +1,45 @@
+
+This is word colors  sample of .py file
+\To high light remaining line
+@ to change colors of line 
+`testing
+` testing
+```test test 
+``` test test
+~test testtest test
+~~~test test
+@ test test
+@@@ test test
+!test test
+! test test
+!!!test test
+$ test test
+$$$test test
+?test test
+???test test
+&test test
+&&& test test
+&&&&test test
+|test test
+|||test test
+% test test
+%%%test test
+^ test test
+^^^test test
+<<< test test
+>>>test test
+*test test
+**test test
+***test test
+---test test
+----test test
+=test test
+===test test
++test test
++++ test test
+++++test test
+
+\ test test
+/test test
+///test test
+
