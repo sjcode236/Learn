503bff6337892ac040d1f20ce8e76caf607de83d
sjcode236 <31339109+sjcode236@users.noreply.github.com>
2018-08-12 02:36:55 -0700

    Create phoneApps.py

---
+++ phoneApps.py
@@ -0,0 +1,40 @@
+
+
+
+===photo , scan ,  editing  
+Lightroom CC
+flicker
+photoscan 
+camscanner
+framatic
+phonto
+
+
+wunderground ==> weather 
+weather channel 
+
+===driving apps
+waze
+
+===cars
+offerup 
+kbb.com
+craigslist
+manheim
+
+
+chess ==> christophe theron
+
+speedsmart ==> dataspeedtest  
+speedtest by ookla
+
+====misc
+yelp 
+google home
+google assistant 
+workflow 
+Ezhuthani ==> മലയാളം  കീബോർഡ് 
+
+
+
+
